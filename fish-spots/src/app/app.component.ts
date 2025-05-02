fa048e9a50382071f056829a27ac4d250b977124
Aiden Martin <daboss4269@gmail.com>
2025-05-02 12:19:46 -0600

    updated app.component

--- fish-spots/src/app/app.component.ts
+++ fish-spots/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DraggableMapComponent } from "./draggable-map/draggable-map.component";
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DraggableMapComponent,NavbarComponent],
+  imports: [RouterOutlet, DraggableMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
