ff5dd1528a9c831aa9fde1d2b9f976eae29597e9
aidenmartin864 <112721864+aidenmartin864@users.noreply.github.com>
2025-05-02 12:55:51 -0600

    Implemented popup and 'Add' button over markers (#1)
    * created popup service and modified marker service to call it.
    
    * popup now appears instantly
    
    * popup now appears instantly
    
    * Update app.component.ts
    
    * Update app.component.html

--- fish-spots/src/app/marker.service.ts
+++ fish-spots/src/app/marker.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Map, LatLng, marker, LayerGroup, layerGroup } from 'leaflet';
+import { Map, LatLng, marker, Marker, LayerGroup, layerGroup, popup, Popup } from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
 
-  constructor() { 
+  constructor() {
     this.currMarker = layerGroup();
   }
 
   private currMarker: LayerGroup;
 
   public addMarker(map: Map, latlng: LatLng): void {
-    
     const m = marker([latlng.lat, latlng.lng]);
+    this.addPopupToMarker(m);
     this.clearCurrMarker();
     this.currMarker.addLayer(m);
+
     this.currMarker.addTo(map);
   }
 
   private clearCurrMarker(): void {
     this.currMarker.clearLayers();
+  }
+
+  private addPopupToMarker(m:Marker): void {
+    m.bindPopup("<button class=\"add-button\">Add</>").on('add', function () {
+      m.openPopup();
+    });
   }
 }
