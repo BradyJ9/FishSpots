c4d810d4daf5006c7a40ab49c740a3620b77b3c8
Aiden Martin <daboss4269@gmail.com>
2025-05-02 14:47:47 -0600

    Added navbar with link to homepage

--- fish-spots/src/app/app.component.ts
+++ fish-spots/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DraggableMapComponent } from "./draggable-map/draggable-map.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DraggableMapComponent],
+  imports: [RouterOutlet, DraggableMapComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fish-spots';
 }
+
