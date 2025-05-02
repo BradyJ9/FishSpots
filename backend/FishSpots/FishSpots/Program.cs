9d13f9d9f879b9aa0e02641e98f9f6f40e5a4342
Brady Bess <BBess@seekwell.com>
2025-05-02 12:51:10 -0600

    Add another project for structure

--- backend/FishSpots/FishSpots/Program.cs
+++ backend/FishSpots/FishSpots/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Dependency Injection
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
