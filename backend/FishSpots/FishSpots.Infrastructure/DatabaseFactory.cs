5d18dc5d8a02b7829ff43ef04a00662206cfafee
Brady Bess <BBess@seekwell.com>
2025-05-02 19:12:20 -0600

    Add in DatabaseFactory and HealthCheck Controller

---
+++ backend/FishSpots/FishSpots.Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using Npgsql;
+
+
+namespace FishSpots.Infrastructure
+{
+    public class DatabaseFactory(IHostEnvironment env, IConfiguration config)
+    {
+        private readonly IHostEnvironment _hostEnvironment = env;
+        private readonly IConfiguration _configuration = config;
+
+        public IDbConnection CreateDbConnection()
+        {
+            if (_hostEnvironment.IsDevelopment() || _hostEnvironment.IsEnvironment("Debug"))
+            {
+                var connString = _configuration.GetConnectionString("PostgresqlConnection");
+                return new NpgsqlConnection(connString);
+            }
+
+            throw new InvalidOperationException("Error identifying dev environment");
+            //TODO: Add in Azure SQL Server conn string and use here
+            //var connString = _configuration.GetConnectionString("AzureServerSqlConnection");
+            //return new SqlConnection();
+        }
+    }
+}
--- backend/FishSpots/FishSpots/Program.cs
+++ backend/FishSpots/FishSpots/Program.cs
@@ -1,3 +1,5 @@
+using FishSpots.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Dependency Injection
-
+builder.Services.AddSingleton<DatabaseFactory>();
 
 var app = builder.Build();
 
