a7618d52ef9410b5cded7a8ec51b891a2de45a58
Aiden Martin <daboss4269@gmail.com>
2025-05-13 16:22:12 -0600

    Catch endpoint implemented

--- backend/FishSpots/FishSpots.Domain/Models/Catch.cs
+++ backend/FishSpots/FishSpots.Domain/Models/Catch.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace FishSpots.Domain.Models
 {
-    internal class Catch
+    public class Catch
     {
+		public int CatchId { get; }
+
+		public int OutingId { get; set; }
+
+		public string Species { get; set; }
+
+		public float CatchLength { get; set; }
+
+		public float CatchWeight {  get; set; }
+
+		public int Likes { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+
+		public DateTime LastUpdatedAt { get; set; }
     }
 }
---
+++ backend/FishSpots/FishSpots.Logic/CatchLogic/ICatchLogic.cs
@@ -0,0 +1,15 @@
+﻿using FishSpots.Domain.Models;
+
+namespace FishSpots.Logic.CatchLogic
+{
+    public interface ICatchLogic
+    {
+        Task DeleteCatchByIdAsync(int catchId);
+        Task<List<Catch>> GetAllCatchesAsync();
+        Task<List<Catch>> GetCatchesByOutingAsync(int outingId);
+        Task<List<Catch>> GetCatchesBySpeciesAsync(string species);
+        Task<Catch?> GetCatchByIdAsync(int catchId);
+        Task UpdateCatchByIdAsync(Catch cat, int catchId);
+        Task InsertCatchAsync(Catch cat);
+    }
+}
