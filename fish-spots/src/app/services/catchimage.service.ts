54ffde3fc53fa3cf223c1b9afb3f1778b793597e
Aiden Martin <daboss4269@gmail.com>
2025-05-20 12:50:10 -0600

    very bad catchImages attempt

---
+++ fish-spots/src/app/services/catchimage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { ApiClientService } from "./apiclient.service";
+import { map, Observable, of } from "rxjs";
+import { CatchImageDto } from "../../model/dto/CatchImageDto";
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class CatchImageService {
+    constructor(private apiClient:ApiClientService){}
+
+    private readonly catchImageUrl = 'CatchImage/';
+
+    public getCatchImageById$(catchId: number | undefined): Observable<CatchImageDto> {
+        if (catchId === undefined) {
+            return of();
+        }
+        // return this.apiClient.get<{ image: CatchImageDto }>(this.catchImageUrl + catchId).pipe(
+        //     map(response => response.image)
+        // );
+        console.log(this.apiClient.get<CatchImageDto>(this.catchImageUrl + catchId))
+        return this.apiClient.get<CatchImageDto>(this.catchImageUrl + catchId);
+    }
+
+}
