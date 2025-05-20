b00b53a802b09a4d29763d35270ea0582339cf8f
Aiden Martin <daboss4269@gmail.com>
2025-05-20 17:09:57 -0600

    Returns array of locationImage urls

--- backend/FishSpots/FishSpots.Logic/LocationImageLogic/ILocationImageLogic.cs
+++ backend/FishSpots/FishSpots.Logic/LocationImageLogic/ILocationImageLogic.cs
@@ -4,7 +4,7 @@ namespace FishSpots.Logic.LocationImageLogic
 {
     public interface ILocationImageLogic
     {
-        Task<LocationImage> GetLocationImageByIdAsync(int locationId);
+        Task<List<LocationImage>> GetLocationImagesByIdAsync(int locationId);
         Task DeleteLocationImageByIdAsync(int imageId);
         Task InsertLocationImageAsync(LocationImage locationImage);
         Task UpdateLocationImageByIdAsync(LocationImage locationImage, int imageId);
