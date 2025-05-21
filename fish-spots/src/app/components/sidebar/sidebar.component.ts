b329d3a99a2e96fce7b15829739d83db516ff665
Aiden Martin <daboss4269@gmail.com>
2025-05-21 10:52:15 -0600

    like button improvements

--- fish-spots/src/app/components/sidebar/sidebar.component.ts
+++ fish-spots/src/app/components/sidebar/sidebar.component.ts
@@ -24,29 +24,34 @@ export class SidebarComponent implements OnInit {
     //TODO: Sort catches by recent (if they aren't already)
     //We can do this through the backend SQL query
     ngOnInit(): void {
-    this.catchService.getAllCatches().subscribe({
-      next: (data: CatchDto[]) => {
-      this.catches = data;
-
-      this.catches.forEach((cat, index) => {
-        if (cat.catchId !== undefined) {
-        this.imageService.getCatchImageById$(cat.catchId).subscribe({
-          next: (imageData) => {
-          this.imageMap[cat.catchId!] = imageData.storagePath;
-          },
-          error: (err) => {
-          console.error(`Error fetching image for catch ${cat.catchId}:`, err);
-          // Remove the catch from the list if it has no image
-          this.catches.splice(index, 1);
+      this.catchService.getAllCatches().subscribe({
+        next: (data: CatchDto[]) => {
+        this.catches = data;
+        this.catches.forEach((cat, index) => {
+          if (cat.catchId !== undefined) {
+          this.imageService.getCatchImageById$(cat.catchId).subscribe({
+            next: (imageData) => {
+            this.imageMap[cat.catchId!] = imageData.storagePath;
+            },
+            error: (err) => {
+            console.error(`Error fetching image for catch ${cat.catchId}:`, err);
+            // Remove the catch from the list if it has no image
+            this.catches.splice(index, 1);
+            }
+          });
           }
         });
+        },
+        error: (err) => {
+        console.error('Error fetching catches:', err);
         }
       });
-      },
-      error: (err) => {
-      console.error('Error fetching catches:', err);
-      }
-    });
+
+      let id = 0;
+      const cards = document.querySelectorAll('card');
+      cards.forEach(function(card) {
+        card.id = (id + 1).toString();
+      });
   }
 
   
@@ -62,5 +67,24 @@ export class SidebarComponent implements OnInit {
     if (isNaN(date.getTime())) return 'Invalid date';
 
     return new Intl.DateTimeFormat('en-US').format(date);
+  }
+
+  likeClicked(numLikes:number, id:number): void {
+    const likeButton = document.getElementById("like-button"+id.toString()) as HTMLImageElement;
+    const likesCount = document.getElementById("likes-count"+id.toString()) as HTMLDivElement;
+    
+    if(likeButton.name == "unclicked"){
+      likeButton.src = "/../../../assets/hearticon-red.png";
+      likesCount.textContent = (numLikes+1) + " likes";
+      this.catchService.incrementLikes(id);
+      likeButton.name = "clicked";
+    }
+    
+    else if(likeButton.name == "clicked"){
+      likeButton.src = "../../../assets/hearticon.png";
+      likesCount.textContent = (numLikes) + " likes";
+      this.catchService.decrementLikes(id);
+      likeButton.name = "unclicked";
+    }
   }
 }
