8071ecaaa9845a52ec3e5627aef0fd6dd177fc3d
Brady Bess <BBess@seekwell.com>
2025-06-18 19:11:49 -0600

    Add in blob storage endpoint in frontend

--- fish-spots/src/environments/environments.prod.ts
+++ fish-spots/src/environments/environments.prod.ts
@@ -3,9 +3,7 @@ export const environment = {
       production: true,
       apiUrl: 'https://fish-spots-hrb5ahbzdqdqarfe.westus2-01.azurewebsites.net/api',
       blob: {
-        accountName: 'your-prod-account-name',
-        sasToken: '?sv=...', // Use SAS token or managed identity in production
-        blobUrl: 'https://your-prod-account-name.blob.core.windows.net'
+        blobUrl: 'https://fishspotsblobstorage.blob.core.windows.net/'
       }
     };
     
